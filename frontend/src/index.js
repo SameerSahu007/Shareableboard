ec9ca0227ba252c785ed644e4d56d77e1065f671
sameer <sameersahu200138@gmail.com>
2023-10-21 14:08:30 +0530

    trying old react router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,28 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Chat from './components/Chat';
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "chat/:roomId",
-    element: <Chat />,
-  }
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/chat/:roomId" element={<Chat />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
